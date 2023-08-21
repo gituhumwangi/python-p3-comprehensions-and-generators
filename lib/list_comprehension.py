bd68cfbdb2570b5bba5e767fb5b73309ab94b819
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-21 12:53:26 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,11 @@
 
 def return_evens(num_list):
     pass
+    even_nums = [n for n in num_list if n % 2 == 0  ]
+    return even_nums
+
 
 def make_exclamation(sentence_list):
     pass
+    exclamation_sentence = [ st + "!" for st in sentence_list]
+    return exclamation_sentence
